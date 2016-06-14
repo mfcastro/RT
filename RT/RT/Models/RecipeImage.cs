240c7158b0e71f1901c8117d54c3c1536c7f854e
mfcastro <mfcastro50@gmail.com>
2016-06-14 15:29:38 -0500

    Updated UI on forms page

---
+++ RT/RT/Models/RecipeImage.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace RT.Models
+{
+	public class RecipeImage
+	{
+		[Key]
+		public int ImageID { get; set; }
+		public int ImageSize { get; set; }
+		public string FileName { get; set; }
+		public byte[] ImageData { get; set; }
+		//[Required(ErrorMessage = "Please select file")]
+		//public HttpPostedFileBase File { get; set; }
+
+		public string File
+		{
+			get
+			{
+				string mimeType = "image/png";
+				string base64 = Convert.ToBase64String(ImageData);
+				return string.Format("data:{0},{1}", mimeType, base64);
+			}
+		}
+	}
+}
